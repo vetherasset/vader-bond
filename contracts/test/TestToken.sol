6c1a5ea47d0b4f8836638c0ba7a187f7d39eda80
0xstormtrooper <0xstormtrooper@protonmail.com>
2021-11-04 02:25:01 -0400

    test token

---
+++ contracts/test/TestToken.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.7.6;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    function mint(address _to, uint _amount) external {
+        _mint(_to, _amount);
+    }
+
+    function burn(address _from, uint _amount) external {
+        _burn(_from, _amount);
+    }
+}
