c143bd2b61633acc565c85a45a5e4981702a46df
0xstormtrooper <0xstormtrooper@protonmail.com>
2021-11-04 02:28:13 -0400

    TestToken decimals

--- contracts/test/TestToken.sol
+++ contracts/test/TestToken.sol
@@ -4,7 +4,13 @@ pragma solidity 0.7.6;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals
+    ) ERC20(name, symbol) {
+        _setupDecimals(decimals);
+    }
 
     function mint(address _to, uint _amount) external {
         _mint(_to, _amount);
