8e8a44d9d53fdbb620236e623911840e34f1d3e0
0xstormtrooper <0xstormtrooper@protonmail.com>
2021-11-10 19:18:33 -0500

    todo

--- contracts/VaderBond.sol
+++ contracts/VaderBond.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.7.6;
+// TODO: use Solidity 0.8 if FixedPoint is not needed
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
