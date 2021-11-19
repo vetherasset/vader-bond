a826247605c959fbcbdfaa8564d3e28fb7c0698b
0xstormtrooper <0xstormtrooper@protonmail.com>
2021-11-18 23:11:54 -0500

    deploy test token script

---
+++ scripts/deploy_test_token.py
@@ -0,0 +1,10 @@
+from brownie import TestToken, accounts, network
+
+
+def main():
+    account = accounts.load("dev")
+
+    net = network.show_active()
+    assert net == "kovan"
+
+    TestToken.deploy("test token", "TEST", 18, {"from": account}, publish_source=True)
