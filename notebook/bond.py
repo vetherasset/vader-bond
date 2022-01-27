f54dd58143463766e429b4860050469cfe908cde
0xstormtrooper <0xstormtrooper@protonmail.com>
2022-01-27 03:05:28 +0000

    ETH bond Kovan

--- notebook/bond.py
+++ notebook/bond.py
@@ -14,7 +14,6 @@
 
 from datetime import datetime
 
-
 def now():
     return datetime.now().timestamp()
 
@@ -24,33 +23,31 @@ def tx(func):
         self = args[0]
         self.block.inc()
         return func(*args, **kwargs)
-
     return inner
 
 
 class Block:
-    def __init__(self, timestamp=now(), number=0):
+    def __init__(self, timestamp = now(), number = 0):
         self.timestamp = timestamp
         self.number = number
-
-    def inc(self, n=1):
+     
+    def inc(self, n = 1):
         self.number += n
         self.timestamp += n * 14
-
 
 DECIMALS = 18
 PRINCIPAL_DECIMALS = 18
 
 
 class Token:
-    def __init__(self, totalSupply=0):
+    def __init__(self, totalSupply = 0):
         self.totalSupply = totalSupply
 
 
 class Treasury:
     def __init__(self):
         pass
-
+    
     # view
     def value_of_token(self, amount):
         return amount
@@ -61,18 +58,18 @@ class Bond:
         self.block = block
         self.payoutToken = payoutToken
         self.treasury = treasury
-
+        
         # terms
         assert terms["control_variable"] > 0
         assert terms["vesting_terms"] >= 10000
         assert terms["max_payout"] <= 1000
-
+        
         self.terms = {
             "control_variable": terms["control_variable"],
             "vesting_term": terms["vesting_terms"],
             "min_price": terms["min_price"],
             "max_payout": terms["max_payout"],
-            "max_debt": terms["max_debt"],
+            "max_debt": terms["max_debt"]
         }
 
         self.total_debt = initial_debt
@@ -84,7 +81,7 @@ class Bond:
             "rate": adj["rate"],
             "target": adj["target"],
             "buffer": adj["buffer"],
-            "last_block": adj["last_block"],
+            "last_block": adj["last_block"]
         }
 
     # view
@@ -102,12 +99,12 @@ class Bond:
     # view
     def current_debt(self):
         return self.total_debt - self.debt_decay()
-
+    
     # view
     def debt_ratio(self):
         return self.current_debt() * 10 ** 18 / self.payoutToken.totalSupply
-
-    # view
+    
+    #view
     def bond_price(self):
         price = self.terms["control_variable"] * self.debt_ratio() / 10 ** 18
         if price < self.terms["min_price"]:
@@ -121,7 +118,7 @@ class Bond:
     # view
     def max_payout(self):
         return self.payoutToken.totalSupply * self.terms["max_payout"] / 1e5
-
+    
     def adjust(self):
         block_can_adjust = self.adjustment["last_block"] + self.adjustment["buffer"]
         rate = self.adjustment["rate"]
@@ -137,30 +134,30 @@ class Bond:
                     self.adjustment["rate"] = 0
 
             self.adjustment["last_block"] = self.block.number
-
+    
     @tx
     def deposit(self, amount):
         self.decay_debt()
         assert self.total_debt < self.terms["max_debt"]
-
+        
         value = self.treasury.value_of_token(amount)
         payout = self.payout_for(value)
-
+        
         assert payout >= 10 ** DECIMALS / 100
         assert payout <= self.max_payout()
-
+        
         self.total_debt += value
-
+        
         price = self.bond_price()
         if price > self.terms["min_price"] and self.terms["min_price"] > 0:
             self.terms["min_price"] = 0
-
+        
         self.adjust()
-
+        
         return payout
 
 
-# In[4]:
+# In[16]:
 
 
 import math
@@ -169,6 +166,7 @@ import json
 BLOCKS_PER_HOUR = 270
 VADER_TOTAL_SUPPLY = (25 * 1e9) * 10 ** DECIMALS
 SALE = 50 * (1e6) * (10 ** DECIMALS)
+VESTING_DAYS = 21
 
 TOKEN_PRICE_USD = 3000.00
 VADER_PRICE_USD = 0.055
@@ -188,7 +186,7 @@ min_price = x * 10 ** PRINCIPAL_DECIMALS
 # bond price = control variable * debt ratio / 1e18
 # debt ratio = current debt * 1e18 / total supply of Vader
 # current debt <= total amount of token to receive
-#
+# 
 # cv = control variable
 # V = total supply of Vader
 # d = current debt
@@ -205,7 +203,7 @@ D = min_price * SALE / 10 ** DECIMALS
 control_variable = min_price * VADER_TOTAL_SUPPLY / D
 
 # --- vesting terms ---
-vesting_terms = BLOCKS_PER_HOUR * 24 * 21
+vesting_terms = BLOCKS_PER_HOUR * 24 * VESTING_DAYS
 
 # --- max payout % (100% = 1e5) ---
 max_payout = math.ceil(MAX_TOKEN_PER_DEPOSIT * v / VADER_TOTAL_SUPPLY * 1e5)
@@ -220,22 +218,25 @@ terms = {
     "vesting_terms": vesting_terms,
     "min_price": min_price,
     "max_payout": max_payout,
-    "max_debt": max_debt,
+    "max_debt": max_debt
 }
 
-adj = {"add": False, "rate": 0, "target": 0, "buffer": 0, "last_block": 0}
+adj = {
+    "add": False,
+    "rate": 0,
+    "target": 0,
+    "buffer": 0,
+    "last_block": 0
+}
 
 print("Max token per deposit:", MAX_TOKEN_PER_DEPOSIT / 10 ** PRINCIPAL_DECIMALS)
 print("Min token before price > min price:", D / 10 ** PRINCIPAL_DECIMALS)
 print("Min price:", terms["min_price"] / 10 ** PRINCIPAL_DECIMALS)
-print(
-    "Price:",
-    terms["control_variable"] * D / VADER_TOTAL_SUPPLY / 10 ** PRINCIPAL_DECIMALS,
-)
+print("Price:", terms["control_variable"] * D / VADER_TOTAL_SUPPLY / 10 ** PRINCIPAL_DECIMALS)
 print("terms:", json.dumps(terms, indent=2))
 
 
-# In[3]:
+# In[17]:
 
 
 block = Block()
@@ -268,23 +269,23 @@ num_buyers = 0
 for i in range(N):
     amount = 0
     payout = 0
-
+    
     # get bond price before buy
     prices.append(b.bond_price())
-
+    
     if random() > 0.499:
         market_price = min(market_price * 1.01, 1000 * min_price)
     else:
         market_price *= 0.99
 
     market_price = min_price
-
+    
     if sold < SALE and random() > 0.9 and b.bond_price() <= 1.05 * market_price:
         r = random()
-        amount = r * MAX_LP_PER_DEPOSIT
+        amount = r * MAX_TOKEN_PER_DEPOSIT
         value = treasury.value_of_token(amount)
         payout = b.payout_for(value)
-
+        
         if payout < 10 ** DECIMALS / 100 or payout >= b.max_payout():
             payout = 0
             amount = 0
@@ -295,7 +296,7 @@ for i in range(N):
             num_buyers += 1
     else:
         block.inc(1)
-
+    
     payouts.append(payout)
     amounts.append(amount)
     market_prices.append(market_price)
@@ -320,22 +321,12 @@ for p in prices:
     price_changes.append(dp)
     p0 = p
 
-
-def sample(f=lambda i: True):
+def sample(f = lambda i: True):
     n = len(prices)
 
     table = PrettyTable()
-    table.field_names = [
-        "block",
-        "price",
-        "price change %",
-        "market price",
-        "amount",
-        "payout",
-        "sold",
-        "% sold",
-    ]
-
+    table.field_names = ["block", "price", "price change %", "market price", "amount", "payout", "sold", "% sold"]
+    
     for i in range(n):
         if f(i):
             price = prices[i] / 10 ** PRINCIPAL_DECIMALS
@@ -345,59 +336,57 @@ def sample(f=lambda i: True):
             market_price = market_prices[i] / 10 ** PRINCIPAL_DECIMALS
             total_payout = total_payouts[i] / 10 ** DECIMALS
             percent_sold = total_payout * 10 ** DECIMALS / SALE * 100
-            table.add_row(
-                [
-                    i,
-                    f"{price:.6f}",
-                    f"{price_change:.4f}",
-                    f"{market_price:.6f}",
-                    f"{amount:.2f}",
-                    f"{payout:.2f}",
-                    f"{total_payout:.2f}",
-                    f"{percent_sold:.2f}",
-                ]
-            )
+            table.add_row([
+                i,
+                f'{price:.6f}',
+                f'{price_change:.4f}',
+                f'{market_price:.6f}',
+                f'{amount:.2f}',
+                f'{payout:.2f}',
+                f'{total_payout:.2f}',
+                f'{percent_sold:.2f}'
+            ])
 
     print(table)
 
-
-print(f"num buyers: {num_buyers}")
+print(f'num buyers: {num_buyers}')
 
 # sample(lambda i: i % (N / 10) == 0)
 sample(lambda i: price_changes[i] > 0 or payouts[i] > 0)
-
+            
 print("--- price ---")
-plt.plot(xs, prices)
+plt.plot(xs, prices) 
 plt.show()
 
 print("--- price change ---")
-plt.plot(xs, price_changes)
+plt.plot(xs, price_changes) 
 plt.show()
 
 print("--- total payout ---")
-plt.plot(xs, total_payouts)
+plt.plot(xs, total_payouts) 
 plt.show()
 
 print("--- payout ---")
-plt.plot(xs, payouts)
+plt.plot(xs, payouts) 
 plt.show()
 
 print("--- amount ---")
-plt.plot(xs, amounts)
+plt.plot(xs, amounts) 
 plt.show()
 
 print("--- market price ---")
-plt.plot(xs, market_prices)
+plt.plot(xs, market_prices) 
 plt.show()
 
 print("--- control variable ---")
-plt.plot(xs, control_vars)
+plt.plot(xs, control_vars) 
 plt.show()
 
 print("--- debt ratio ---")
-plt.plot(xs, debt_ratios)
+plt.plot(xs, debt_ratios) 
 plt.show()
 
 print("--- total debt ---")
-plt.plot(xs, total_debts)
+plt.plot(xs, total_debts) 
 plt.show()
+
--- scripts/deploy_vader_bond.py
+++ scripts/deploy_vader_bond.py
@@ -3,14 +3,20 @@ import os
 
 CONTRACTS = {
     "kovan": {
-        "treasury": "0xda65ebebEf219f229E69E25a70fE6A8443Ee1aC6",
+        "treasury": "0x666266f24E17d9ab7bCb25715C75146143E16c39",
         "vader": "0xB46dbd07ce34813623FB0643b21DCC8D0268107D",
-        "principal": "0xC42706E83433580dd8d865a30e2Ae61082056007",
+        # Uniswap ETH - Vader LP
+        # "principal": "0xC42706E83433580dd8d865a30e2Ae61082056007",
+        # WETH
+        "principal": "0xd0A1E359811322d97991E03f863a0C30C2cF029C",
     },
     "mainnet": {
         "treasury": "0x8a2afC7a4c2C19E81a79D9158d6bca3858a87B73",
         "vader": "0x2602278EE1882889B946eb11DC0E810075650983",
-        "principal": "0x452c60e1E3Ae0965Cd27dB1c7b3A525d197Ca0Aa",
+        # Uniswap ETH - Vader LP
+        # "principal": "0x452c60e1E3Ae0965Cd27dB1c7b3A525d197Ca0Aa",
+        # WETH
+        "principal": "",
     },
 }
 
